dd3ea1ce594b690c728382a7fb4cac32c2980b63
Jason Vanderslice <jlvanderslice@gmail.com>
2017-01-17 12:23:19 -0600

    Lecture examples of nested routes

--- js/Welcome.js
+++ js/Welcome.js
@@ -4,6 +4,7 @@ export default React.createClass({
   render() {
     return (
       <section>
+        <h1>Welcome to my super-awesome-blog</h1>
       </section>
     )
   }
--- js/index.js
+++ js/index.js
@@ -1,8 +1,25 @@
 import React from 'react'
 import { render } from 'react-dom'
-import Home from './Home'
+import { Router, Route, hashHistory } from 'react-router'
+import App from './App'
+import Welcome from './Welcome'
+import Dashboard from './Dashboard'
+import Post from './Post'
 
+// https://github.com/ReactTraining/react-router
+// https://github.com/ReactTraining/react-router/blob/master/docs/API.md
+// https://developer.mozilla.org/en-US/docs/Web/API/History_API
+//  Push state allows us to have a meaningful browser history
+//    in order to use the browser's back button
 render(
-  ( <Home /> ),
+  (
+    <Router history={hashHistory}>
+      <Route component={App}>
+        <Route path="/" component={ Welcome } />
+        <Route path="/dashboard" component={ Dashboard } />
+        <Route path="/post" component={ Post } />
+      </Route>
+    </Router>
+  ),
   document.getElementById('app')
 )
